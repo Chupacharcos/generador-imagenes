1c92cabdb8ed86c75af94d14b0afcf0cde012277
Chupacharcos <48329953+Chupacharcos@users.noreply.github.com>
2026-03-14 16:26:14 +0000

    security: restringir CORS al dominio del portfolio

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Generador de Imágenes IA", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://adrianmoreno-dev.com", "http://127.0.0.1", "http://localhost"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
